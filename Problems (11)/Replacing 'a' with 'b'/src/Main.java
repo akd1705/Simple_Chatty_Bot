b345423fa0020f0f542396416c73099083174070
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2020-02-16 17:53:38 +0530

    Program for replacing 'a' with 'b' in a user input String.

---
+++ Problems (11)/Replacing 'a' with 'b'/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        String str1=str.replace('a','b');
+        System.out.println(str1);
+        // put your code here
+    }
+}
