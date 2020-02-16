a2fafccf59bf41946cfa0a95e8204b10d1535aec
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2020-02-17 00:06:31 +0530

    To check only one parameter is positive.

---
+++ Problems (16)/Exactly one parameter is positive/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        int num3=scanner.nextInt();
+        if (num1>0 && num2<=0 && num3<=0)
+        {
+            System.out.println(true);
+        }
+        else if (num1<=0 && num2>0 && num3<=0)
+        {
+            System.out.println(true);
+        }
+        else if (num1<=0 && num2<=0 && num3>0)
+        {
+            System.out.println(true);
+        }
+        else
+        {
+            System.out.println(false);
+        }
+        // put your code here
+    }
+}
